39fd15e6b592f2c6345ce99b334c152b8d4ff1ee
dfshaw1594 <31594092+dfshaw1594@users.noreply.github.com>
2019-02-20 16:28:05 -0800

    Get public ip

---
+++ get_public_ip.py
@@ -0,0 +1,13 @@
+#  Gets public ip  
+
+def get_public_ip(request_target):
+    grabber = urllib2.build_opener()
+    grabber.addheaders = [('User-agent', 'Mozilla/5.0')]
+    try:
+        public_ip_address = grabber.open(target_url).read()
+    except urllib2.HTTPError, error:
+        print("There was an error trying to get your Public IP:  %s") % (error)
+    except urllib2.URLError, error:
+        print("There was an error trying to get your Public IP:  %s") % (error)
+    return public_ip_address
+    
