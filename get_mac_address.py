14943de747785e2572db17f91d542b301d2a516c
dfshaw1594 <31594092+dfshaw1594@users.noreply.github.com>
2019-03-11 13:06:54 -0700

    Update get_mac_address.py

--- get_mac_address.py
+++ get_mac_address.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-# Get mac address
+#  Get mac address
+#  Change interface if necessary
 
 def getHwAddr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
     return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+
+print(getHwAddr('eth0'))
